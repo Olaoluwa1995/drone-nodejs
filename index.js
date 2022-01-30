e676736bcbe54d14a1471521cb431ff6f8c1abe2
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-30 15:28:10 +0100

    Added database connection

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const { port } = require("./config");
+const { port } = require('./config');
+const database = require('./config/database');
 
 const startServer = async () => {
     try {
         const app = express();
         app.use(cors());
         app.use(morgan('dev'));
-        
+        await database.connect();
         app.listen(port, () => {
-            console.log('App is running!');
+            console.log(`App is running on ${port}!`);
         })
     } catch (error) {
         console.log(error);
