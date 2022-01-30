6db86992a6dd2bea2039aef1c11505c498b3613b
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-30 22:42:33 +0100

    Updated setup

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 const { port } = require('./config');
 const database = require('./config/database');
+const route = require('./routes');
 
 const startServer = async () => {
     try {
         const app = express();
         app.use(cors());
         app.use(morgan('dev'));
+        app.use(express.json());
+
+        app.use(route)
         await database.connect();
         app.listen(port, () => {
             console.log(`App is running on ${port}!`);
