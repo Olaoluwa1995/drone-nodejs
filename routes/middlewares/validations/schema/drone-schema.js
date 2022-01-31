dded5eb6fc9290c407d08075b7e22e9820aba538
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-31 15:53:36 +0100

    Added medication schema

--- routes/middlewares/validations/schema/drone-schema.js
+++ routes/middlewares/validations/schema/drone-schema.js
@@ -1,4 +1,4 @@
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
 
 module.exports = {
     droneSchema: () => [
@@ -12,13 +12,36 @@ module.exports = {
             .notEmpty(),
         body('weight')
             .isInt({ max: 500 })
+            .withMessage('weight must be an integer within 500')
             .notEmpty(),
         body('batteryCapacity')
             .isInt({ max: 100 })
+            .withMessage('batteryCapacity must be an integer within 100')
             .notEmpty(),
         body('state')
             .isString()
             .isIn(['IDLE', 'LOADING', 'LOADED', 'DELIVERING', 'DELIVERED', 'RETURNING'])
             .notEmpty(),
+    ],
+
+    medSchema: () => [
+        param('droneId')
+            .isUUID()
+            .withMessage('droneId must be a valid UUID')
+            .notEmpty(),
+        body('name')
+            .isString()
+            .matches(/^[A-Za-z0-9_-]*$/)
+            .withMessage('name can only contain letters, numbers, -, _')
+            .notEmpty(),
+        body('weight')
+            .isInt()
+            .withMessage('weight must be an integer')
+            .notEmpty(),
+        body('code')
+            .isString()
+            .matches(/^[A-Z0-9_]*$/)
+            .withMessage('code can only upper case letters, underscore and numbers')
+            .notEmpty()
     ]
 }
