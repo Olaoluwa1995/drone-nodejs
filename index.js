ebfae7176a2615835cbc36b24cc0ce5cfc1dae04
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-31 15:55:05 +0100

    Set app to use file upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const { port } = require('./config');
 const database = require('./config/database');
 const route = require('./routes');
@@ -11,6 +12,9 @@ const startServer = async () => {
         const app = express();
         app.use(cors());
         app.use(morgan('dev'));
+        app.use(fileUpload({
+            createParentPath: true,
+        }));
         app.use(express.json());
 
         app.use(route)
