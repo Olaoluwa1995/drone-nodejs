c23cd24ab8645519368d1f80325a62624e0586a5
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-31 18:03:14 +0100

    Added new routes

--- routes/drone-route.js
+++ routes/drone-route.js
@@ -1,10 +1,13 @@
-const { droneSchema, medSchema } = require('./middlewares/validations/schema/drone-schema');
+const { droneSchema, medSchema, droneIdSchema } = require('./middlewares/validations/schema/drone-schema');
 const validate = require('./middlewares/validations');
-const { register, loadDrone } = require('../controllers/drone-controller');
+const { register, loadDrone, getDroneMedItem, getAvailableDrones, getDroneBatteryLevel } = require('../controllers/drone-controller');
 
 const router = require('express').Router();
 
-router.post('/register', droneSchema(), validate, register);
+router.post('/', droneSchema(), validate, register);
+router.get('/', getAvailableDrones);
+router.get('/:droneId/battery', getDroneBatteryLevel);
+router.get('/:droneId/med', droneIdSchema(), validate, getDroneMedItem);
 router.post('/:droneId/load', medSchema(), validate, loadDrone);
 
 module.exports = router;
