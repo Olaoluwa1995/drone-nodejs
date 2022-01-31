f449e9ed5a4153b9076da6cbb4b65eff52c970b9
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-31 15:53:16 +0100

    Added drone-medication functionalities

--- repositories/drone-repository.js
+++ repositories/drone-repository.js
@@ -1,4 +1,4 @@
-const { Drone } = require("../models");
+const { Drone, Medication } = require("../models");
 
 class DroneRepository {
     static async create (newDrone) {
@@ -6,9 +6,24 @@ class DroneRepository {
         return drone;
     }
 
+    static async createMed (newMed) {
+        const med = await Medication.create(newMed);
+        return med;
+    }
+
+    static async findById (droneId) {
+        const drone = await Drone.findOne({ droneId });
+        return drone;
+    }
+
     static async findBySerialNumber (serialNumber) {
         const drone = await Drone.findOne({serialNumber});
         return drone;
+    }
+
+    static async findDroneMed (droneId) {
+       const meds = await Medication.find({ droneId });
+       return meds
     }
 }
 
