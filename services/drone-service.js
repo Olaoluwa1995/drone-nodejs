2dc1de793819f2c1a7c617038c3f53358588f2ff
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-01-31 18:02:32 +0100

    Implemented battery-checker and state-update functionalities

--- services/drone-service.js
+++ services/drone-service.js
@@ -1,4 +1,5 @@
 const { v4 } = require('uuid');
+const logger = require('../config/logger');
 const DroneRepository = require('../repositories/drone-repository');
 const { serviceResponse } = require('../utilities/response');
 const { uploadImage } = require('../utilities/upload');
@@ -27,20 +28,71 @@ class DroneService {
         try {
             const { weight } = medToLoad;
             if(files === undefined) return serviceResponse(422, 'Load Item image is required');
-            const drone = await DroneRepository.findDroneById(droneId);
+            const drone = await DroneRepository.findById(droneId);
             if(!drone) return serviceResponse(404, 'Invalid drone id');
 
             const loadedItem = await DroneRepository.findDroneMed(droneId);
-            const alreadyLoadedWeight = this.sum(loadedItem, 'weight');
-            const currentLoadWeight = alreadyLoadedWeight + weight;
+            const alreadyLoadedWeight = await this.sum(loadedItem, 'weight');
+            const currentLoadWeight = alreadyLoadedWeight + parseInt(weight);
+            if(drone.batteryCapacity < 25) return serviceResponse(400, 'Drone Battery is below 25%');
             if(currentLoadWeight > drone.weight) 
                 return serviceResponse(409, `Opps! Drone avaible space is ${drone.weight - alreadyLoadedWeight}gr`);
-
+            if(currentLoadWeight === drone.weight)  {
+                await drone.updateOne({ state: 'LOADED' });
+            } else {
+                await drone.updateOne({state: 'LOADING'});
+            }
             const image = await uploadImage(files.image);
             
-            const loadMed = await DroneRepository.createMed({...medToLoad, image});
+            const loadMed = await DroneRepository.createMed({...medToLoad, image, droneId});
             return serviceResponse(201, 'Successfully loaded drone with medication', loadMed);
-        } catch (err) {
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getLoadedDroneItems (droneId) {
+        try {
+            const droneExist = await DroneRepository.findById(droneId);
+            if(!droneExist) return serviceResponse(404, 'Drone not found');
+
+            const loadedItems = await DroneRepository.findDroneMed(droneId);
+            return serviceResponse(200, 'Successfully feched drone medications', loadedItems);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getDroneBatteryLevel (droneId) {
+        try {
+            const droneExist = await DroneRepository.findById(droneId);
+            if(!droneExist) return serviceResponse(404, 'Drone not found');
+            const batteryLevel = {
+                batteryCapacity: droneExist.batteryCapacity,
+            }
+            return serviceResponse(200, 'Successfully feched drone battery', batteryLevel);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getAvailableDrones () {
+        try {
+            const availableDrones = await DroneRepository.findAvailableDrones();
+            return serviceResponse(200, 'Succcessfully fetched available drones', availableDrones);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async logBatteryLevel () {
+        try {
+            const drones = await DroneRepository.findAll();
+            drones.forEach((drone) => {
+                logger.info(`${drone.serialNumber} battery level is currently ${drone.batteryCapacity}`);
+            })
+            return;
+        } catch (error) {
             console.log(err);
         }
     }
