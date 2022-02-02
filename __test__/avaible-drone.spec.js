419111987569cb8fa941d1909f19040990caa690
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-02-02 05:58:13 +0100

    Created available-drone test

---
+++ __test__/avaible-drone.spec.js
@@ -0,0 +1,57 @@
+const request = require('supertest');
+const app = require('../app');
+const db = require('../config/database');
+const { Drone } = require('../models');
+
+const droneTestCase = [
+    {
+        droneId: "5d137184-ba1f-4071-a8bd-f4968e82750f",
+        serialNumber: "OADJFKAJFIACNDJAS",
+        model: "Lightweight",
+        weight: 200,
+        state: 'IDLE',
+        batteryCapacity: 100
+    },
+    {
+        droneId: "5d137184-ba1f-4071-a8bd-f4968e827503",
+        serialNumber: "OADJFKAJFIACNDJAS",
+        model: "Lightweight",
+        weight: 500,
+        state: 'LOADING',
+        batteryCapacity: 100
+    },
+    {
+        droneId: "5d137184-ba1f-4071-a8bd-f4968e827503",
+        serialNumber: "OADJFKAJFIACNDJAS",
+        model: "Lightweight",
+        weight: 500,
+        state: 'LOADING',
+        batteryCapacity: 100
+    }
+]
+
+beforeAll(async () => {
+    await db.connect();
+    await Drone.insertMany(droneTestCase);
+});
+
+
+afterAll(async () => {
+    try {
+        await Drone.deleteMany({});
+        await db.disconnect();
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+describe('--- Get Available Drone ---', () => {
+    it('should return status 200', async () => {
+        const response = await request(app)
+            .get('/drone');
+            
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.status).toEqual('success');
+        expect(response.body.data.length).toEqual(2);
+    });
+});
