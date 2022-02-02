3ce717986c4b382e4d7abd23b28c97fcf280c744
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-02-02 05:56:23 +0100

    Added test uploads folder

--- utilities/upload.js
+++ utilities/upload.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
+
+const uploadLocation = process.env.NODE_ENV === 'test'
+  ? './uploads/test'
+  : './uploads';
+  
 exports.uploadImage = async (fileToUpload) => {
   try {
     // Generate upload key and get file type
     const { name } = fileToUpload;
     const splitFileName = name.split('.');
     const ext = splitFileName[splitFileName.length - 1];
+    
 
-    fileToUpload.mv(path.join('./uploads', `${Date.now()}.${ext}`), (err) => {
+    fileToUpload.mv(path.join(uploadLocation, `${Date.now()}.${ext}`), (err) => {
       if (err) {
         return false;
       }
